43f0a1e1d714746efe52e52448c84e98b8ad1bdf
Steve <playzzsteven@gmail.com>
2023-01-16 16:19:43 -0500

    fix some rei stuffs

--- src/main/java/com/chefsdelights/farmersrespite/integration/rei/REIPlugin.java
+++ src/main/java/com/chefsdelights/farmersrespite/integration/rei/REIPlugin.java
@@ -1,18 +1,11 @@
 package com.chefsdelights.farmersrespite.integration.rei;
 
-//import com.farmersrespite.client.gui.KettleScreen;
-//import com.farmersrespite.common.block.entity.container.KettleContainer;
-//import com.farmersrespite.common.crafting.KettleRecipe;
-//import com.farmersrespite.core.FarmersRespite;
-//import com.farmersrespite.core.registry.FRItems;
-//import com.farmersrespite.integration.jei.category.BrewingRecipeCategory;
-
 import com.chefsdelights.farmersrespite.client.gui.KettleScreen;
 import com.chefsdelights.farmersrespite.common.crafting.KettleRecipe;
 import com.chefsdelights.farmersrespite.core.FarmersRespite;
 import com.chefsdelights.farmersrespite.core.registry.FRBlocks;
-//import com.chefsdelights.farmersrespite.integration.rei.category.BrewingRecipeCategory;
-//import com.chefsdelights.farmersrespite.integration.rei.category.BrewingRecipeDisplay;
+import com.chefsdelights.farmersrespite.integration.rei.category.BrewingRecipeCategory;
+import com.chefsdelights.farmersrespite.integration.rei.category.BrewingRecipeDisplay;
 import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
@@ -26,30 +19,30 @@ import net.fabricmc.api.Environment;
 import net.minecraft.MethodsReturnNonnullByDefault;
 
 //@ParametersAreNonnullByDefault
-//@Environment(EnvType.CLIENT)
-//@MethodsReturnNonnullByDefault
+@Environment(EnvType.CLIENT)
+@MethodsReturnNonnullByDefault
 @SuppressWarnings("unused")
 public class REIPlugin implements REIClientPlugin {
-//    public static final CategoryIdentifier<BrewingRecipeDisplay> BREWING = CategoryIdentifier.of(FarmersRespite.MOD_ID, "brewing");
-//
-//    public static Rectangle centeredIntoRecipeBase(Point origin, int width, int height) {
-//        return centeredInto(new Rectangle(origin.x, origin.y, 150, 66), width, height);
-//    }
-//
-//    public static Rectangle centeredInto(Rectangle origin, int width, int height) {
-//        return new Rectangle(origin.x + (origin.width - width) / 2, origin.y + (origin.height - height) / 2, width, height);
-//    }
-//
-//    public void registerCategories(CategoryRegistry registry) {
-//        registry.add(new BrewingRecipeCategory());
-//        registry.addWorkstations(BREWING, EntryStacks.of(FRBlocks.KETTLE));
-//    }
-//
-//    public void registerDisplays(DisplayRegistry registry) {
-//        registry.registerRecipeFiller(KettleRecipe.class, KettleRecipe.TYPE, BrewingRecipeDisplay::new);
-//    }
-//
-//    public void registerScreens(ScreenRegistry registry) {
-//        registry.registerContainerClickArea(new Rectangle(62, 25, 40, 17), KettleScreen.class, BREWING);
-//    }
+    public static final CategoryIdentifier<BrewingRecipeDisplay> BREWING = CategoryIdentifier.of(FarmersRespite.MOD_ID, "brewing");
+
+    public static Rectangle centeredIntoRecipeBase(Point origin, int width, int height) {
+        return centeredInto(new Rectangle(origin.x, origin.y, 150, 66), width, height);
+    }
+
+    public static Rectangle centeredInto(Rectangle origin, int width, int height) {
+        return new Rectangle(origin.x + (origin.width - width) / 2, origin.y + (origin.height - height) / 2, width, height);
+    }
+
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new BrewingRecipeCategory());
+        registry.addWorkstations(BREWING, EntryStacks.of(FRBlocks.KETTLE));
+    }
+
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.registerRecipeFiller(KettleRecipe.class, KettleRecipe.TYPE, BrewingRecipeDisplay::new);
+    }
+
+    public void registerScreens(ScreenRegistry registry) {
+        registry.registerContainerClickArea(new Rectangle(62, 25, 40, 17), KettleScreen.class, BREWING);
+    }
 }
