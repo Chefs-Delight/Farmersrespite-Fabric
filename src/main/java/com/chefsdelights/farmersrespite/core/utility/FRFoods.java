df02f9a2dfb63db1a28ca43b589fece5bc73a251
mrsterner <simon@sterner.dev>
2023-08-12 23:19:18 +0200

    always drinkable fix #26

--- src/main/java/com/chefsdelights/farmersrespite/core/utility/FRFoods.java
+++ src/main/java/com/chefsdelights/farmersrespite/core/utility/FRFoods.java
@@ -8,26 +8,26 @@ import net.minecraft.world.food.FoodProperties;
 
 public class FRFoods {
     // Drinks (mostly for effects)
-    public static final FoodProperties GREEN_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 0), 1.0F).build();
-    public static final FoodProperties YELLOW_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 3600, 0), 1.0F).build();
-    public static final FoodProperties BLACK_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.POISON, 200, 0), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 200, 0), 1.0F).build();
-    public static final FoodProperties DANDELION_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(EffectsRegistry.COMFORT.get(), 3600, 0), 1.0F).build();
-    public static final FoodProperties PURULENT_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.WEAKNESS, 600, 0), 1.0F).build();
-    public static final FoodProperties COFFEE = (new FoodProperties.Builder()).effect(new MobEffectInstance(FREffects.CAFFEINATED, 6000, 1), 1.0F).build();
+    public static final FoodProperties GREEN_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 0), 1.0F).build();
+    public static final FoodProperties YELLOW_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 3600, 0), 1.0F).build();
+    public static final FoodProperties BLACK_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.POISON, 200, 0), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 200, 0), 1.0F).build();
+    public static final FoodProperties DANDELION_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(EffectsRegistry.COMFORT.get(), 3600, 0), 1.0F).build();
+    public static final FoodProperties PURULENT_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.WEAKNESS, 600, 0), 1.0F).build();
+    public static final FoodProperties COFFEE = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(FREffects.CAFFEINATED, 6000, 1), 1.0F).build();
 
-    public static final FoodProperties LONG_GREEN_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DIG_SPEED, 5400, 0), 1.0F).build();
-    public static final FoodProperties LONG_YELLOW_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 5400, 0), 1.0F).build();
-    public static final FoodProperties LONG_BLACK_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.POISON, 300, 0), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 300, 0), 1.0F).build();
-    public static final FoodProperties LONG_DANDELION_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(EffectsRegistry.COMFORT.get(), 5400, 0), 1.0F).build();
-    public static final FoodProperties LONG_COFFEE = (new FoodProperties.Builder()).effect(new MobEffectInstance(FREffects.CAFFEINATED, 12000, 0), 1.0F).build();
-    public static final FoodProperties LONG_APPLE_CIDER = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.ABSORPTION, 1800, 0), 1.0F).build();
+    public static final FoodProperties LONG_GREEN_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DIG_SPEED, 5400, 0), 1.0F).build();
+    public static final FoodProperties LONG_YELLOW_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 5400, 0), 1.0F).build();
+    public static final FoodProperties LONG_BLACK_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.POISON, 300, 0), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 300, 0), 1.0F).build();
+    public static final FoodProperties LONG_DANDELION_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(EffectsRegistry.COMFORT.get(), 5400, 0), 1.0F).build();
+    public static final FoodProperties LONG_COFFEE = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(FREffects.CAFFEINATED, 12000, 0), 1.0F).build();
+    public static final FoodProperties LONG_APPLE_CIDER = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.ABSORPTION, 1800, 0), 1.0F).build();
 
-    public static final FoodProperties STRONG_GREEN_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DIG_SPEED, 1800, 1), 1.0F).build();
-    public static final FoodProperties STRONG_YELLOW_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1800, 1), 1.0F).build();
-    public static final FoodProperties STRONG_BLACK_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.POISON, 100, 1), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 100, 1), 1.0F).build();
-    public static final FoodProperties STRONG_PURULENT_TEA = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.WEAKNESS, 300, 1), 1.0F).build();
-    public static final FoodProperties STRONG_COFFEE = (new FoodProperties.Builder()).effect(new MobEffectInstance(FREffects.CAFFEINATED, 3000, 2), 1.0F).build();
-    public static final FoodProperties STRONG_APPLE_CIDER = (new FoodProperties.Builder()).effect(new MobEffectInstance(MobEffects.ABSORPTION, 600, 1), 1.0F).build();
+    public static final FoodProperties STRONG_GREEN_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DIG_SPEED, 1800, 1), 1.0F).build();
+    public static final FoodProperties STRONG_YELLOW_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1800, 1), 1.0F).build();
+    public static final FoodProperties STRONG_BLACK_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.POISON, 100, 1), 1.0F).effect(new MobEffectInstance(FREffects.CAFFEINATED, 100, 1), 1.0F).build();
+    public static final FoodProperties STRONG_PURULENT_TEA = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.WEAKNESS, 300, 1), 1.0F).build();
+    public static final FoodProperties STRONG_COFFEE = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(FREffects.CAFFEINATED, 3000, 2), 1.0F).build();
+    public static final FoodProperties STRONG_APPLE_CIDER = (new FoodProperties.Builder()).alwaysEat().effect(new MobEffectInstance(MobEffects.ABSORPTION, 600, 1), 1.0F).build();
 
     // Basic Foods
     public static final FoodProperties ROSE_HIP_PIE_SLICE = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.3f).fast().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 300, 0), 1.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 300, 0), 1.0F).build();
